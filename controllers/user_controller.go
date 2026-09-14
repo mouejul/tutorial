5f7922669a951e4eefc7a231b52e7e4f5c0be8c3
mouejul <mahesaconsole@gmail.com>
2026-09-14 09:41:14 +0700

    fix: resolve error in auth

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,7 +1,13 @@
 package controllers
 
-	import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
+	"example.com/event-app/config"
+	"example.com/event-app/models"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthInputRegister struct {
 	Name     string `json:"name" binding:"required"`
@@ -14,9 +20,51 @@ type AuthInputLogin struct {
 	Password string `json:"password" binding:"required"`
 }
 
-func RegisterUser(context *gin.Context){
+func RegisterUser(context *gin.Context) {
 	var input AuthInputRegister
 
 	// validation
-	err := context.ShouldBindBodyWithJSON()
+	err := context.ShouldBindBodyWithJSON(&input)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	hashedPassword, errHash := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if errHash != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Gagal enkripsi password",
+		})
+		return
+	}
+
+	// simpan ke db
+	user := models.User{
+		Name:     input.Name,
+		Email:    input.Email,
+		Password: string(hashedPassword),
+	}
+
+	userCreated := config.DB.Create(&user).Error
+
+	if userCreated != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Email Sudah terdaftar",
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated,
+		gin.H{
+			"message": "Berhasil register",
+			"user": gin.H{
+				"id":     user.ID,
+				"name":   user.Name,
+				"email":  user.Email,
+				"events": user.Events,
+			},
+		})
 }
